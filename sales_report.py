43054ddff6bda71588f245f8d3338a2b01a28315
NOURSAR <96560657+NOURSAR@users.noreply.github.com>
2022-02-19 06:25:33 +0000

    Improved the code: created a functio

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,29 @@
 """Generate sales report showing total melons each salesperson sold."""
 
+def sales_report(file):
+    ''' Calculating how many melons did each salesperson sold and printing it out'''
 
-salespeople = []
-melons_sold = []
+    salespeople = [] #initiating empty lists to append data later
+    melons_sold = []
+    report_file = open(file)#open a file
+    for line in report_file:#looping in the file by line
+       line = line.rstrip()#striping the white space at the end of each line
+       entries = line.split('|')#spliting the words in the lines by '|'
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+       salesperson = entries[0]#indexing where is the salesperson reference in the line
+       melons = int(entries[2])#indixing the melons in the line
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+       if salesperson in salespeople:#looping through the empty list to check for salesperson extracted
+           position = salesperson.index(salesperson)
+           melons_sold[position] += melons #add all melons for same salesperson from different positions
+       else:
+          salespeople.append(salesperson)# if salesperson found once in the file add that person to the list
+          melons_sold.append(melons) #if melons for salesperson found once append it to the list
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
-
-
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+    for i in range(len(salespeople)): # looping through the salespersons list to print the name and,
+        print(f'{salespeople[i]} sold {melons_sold[i]} melons') #print how many melons each salesperson slod
+    report_file.close()
+sales_report("sales-report.txt")
+#a way to improve the code:
+#using functions to organize the code
+#closing the file
